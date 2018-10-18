42330136da3a63fea7ffd981370e671cd741d4a6
Artem <artem.s@2muchcoffee.com>
2018-10-18 10:41:36 +0300

    melistone12

--- src/app/todos/all-items/all-items.component.ts
+++ src/app/todos/all-items/all-items.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { TodoItems } from '../../core/models/todo-items';
+import { TodoItemsService } from '../../core/services/todo-items.service';
 
 @Component({
   selector: 'app-all-items',
   templateUrl: './all-items.component.html',
   styleUrls: ['./all-items.component.scss']
 })
-export class AllItemsComponent  {
+export class AllItemsComponent implements OnInit {
+  todoItems: TodoItems[];
+
+  constructor( private todoItemsService: TodoItemsService ) {}
+
+  ngOnInit() {
+    this.todoItems = this.todoItemsService.todoItems;
+  }
 }
