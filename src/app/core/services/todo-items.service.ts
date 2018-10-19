052fbdb4258aee354c111de700f35d3d90584518
Artem <artem.s@2muchcoffee.com>
2018-10-19 11:23:57 +0300

    feat(app/todos/todo-items/, app/core/services) in service add deleteByid, change todo-items.component.html, add feat subscription to change deleteItems  in todo-items.component.ts

--- src/app/core/services/todo-items.service.ts
+++ src/app/core/services/todo-items.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 import { TODO_ITEMS } from '../mock/mock-todo-items';
 import { TodoItems } from '../models/todo-items';
@@ -8,4 +9,15 @@ import { TodoItems } from '../models/todo-items';
 })
 export class TodoItemsService {
   todoItems: TodoItems[] = TODO_ITEMS;
+  updateTodoItems = new Subject<TodoItems[]>();
+
+  deleteTodoItemById(id: number) {
+    const updateTodoItems = this.todoItems.filter(item => item.id !== id);
+    this.todoItems = updateTodoItems;
+    this.updateTodoItems.next([...this.todoItems]);
+  }
+
+  getUpdateTodoItems() {
+    return this.updateTodoItems.asObservable();
+  }
 }
--- src/app/todos/todo-items/todo-items.component.ts
+++ src/app/todos/todo-items/todo-items.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { TodoItems } from '../../core/models/todo-items';
 import { TodoItemsService } from '../../core/services/todo-items.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-todo-items',
   templateUrl: './todo-items.component.html',
   styleUrls: ['./todo-items.component.scss']
 })
-export class TodoItemsComponent implements OnInit {
+export class TodoItemsComponent implements OnInit, OnDestroy {
   todoItems: TodoItems[];
+  itemsSub: Subscription;
   data;
 
   constructor(private todoItemsService: TodoItemsService,
@@ -19,5 +21,18 @@ export class TodoItemsComponent implements OnInit {
   ngOnInit() {
     this.todoItems = this.todoItemsService.todoItems;
     this.data = this.route.snapshot.data.value;
+
+    this.itemsSub = this.todoItemsService.getUpdateTodoItems()
+    .subscribe((items: TodoItems[]) => {
+      this.todoItems = items;
+    });
+  }
+
+  deleteTodoItemById(itemId: number) {
+    this.todoItemsService.deleteTodoItemById(itemId);
+  }
+
+  ngOnDestroy() {
+    this.itemsSub.unsubscribe();
   }
 }
