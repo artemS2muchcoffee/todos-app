0c9c7dd0e15fbd61a127ad23ae0a0e6478c1269d
Artem <artem.s@2muchcoffee.com>
2018-11-21 12:20:29 +0200

    feat(app/ngxs/todo-items/todo-items.actions.ts)milestone8

--- src/app/ngxs/todo-items/todo-items.actions.ts
+++ src/app/ngxs/todo-items/todo-items.actions.ts
@@ -23,3 +23,79 @@ export class FetchTodoItemFailed {
   ) {
   }
 }
+
+
+export class AddTodoItem {
+  static readonly type = '[Header] Add Todo Item';
+
+  constructor(
+    public readonly payload: TodoItem
+  ) {
+  }
+}
+
+export class AddTodoItemSuccessfully {
+  static readonly type = '[Todo Items] Add Todo Item Successfully';
+
+  constructor(
+    public readonly payload: TodoItem
+  ) {
+  }
+}
+
+export class AddTodoItemFailed {
+  static readonly type = '[Todo Items State] Add Todo Item Failed';
+
+  constructor(
+    public readonly payload: HttpErrorResponse
+  ) {
+  }
+}
+export class DeleteTodoItem {
+  static readonly type = '[Todo Items] Delete Todo Item';
+
+  constructor(
+    public readonly payload: number
+  ) {
+  }
+}
+
+export class DeleteTodoItemSuccessfully {
+  static readonly type = '[Todo Items] Delete Todo Item Successfully';
+
+  constructor(
+    public readonly payload: number
+  ) {
+  }
+}
+
+export class DeleteTodoItemFailed {
+  static readonly type = '[Todo Items] Delete Todo Item Failed';
+
+  constructor(
+    public readonly payload: HttpErrorResponse
+  ) {
+  }
+}
+
+
+export class ToggleTodoItemsComplete {
+  static readonly type = '[Todo Items] Toggle Todo Item Complete';
+
+  constructor(public readonly payload: number) {
+  }
+}
+
+export class ToggleTodoItemCompleteSuccessfully {
+  static readonly type = '[Todo Items] Toggle Todo Item Complete Successfully';
+
+  constructor(public readonly payload: TodoItem) {
+  }
+}
+
+export class ToggleTodoItemCompleteFailed {
+  static readonly type = '[Todo Items] Toggle Todo Item Complete Failed';
+
+  constructor(public readonly payload: HttpErrorResponse) {
+  }
+}
