f6101649f138cca231ebb8359c1d37d51eec49c3
Artem <artem.s@2muchcoffee.com>
2018-11-21 10:50:15 +0200

    feat(app/ngxs/todo-items/todo-items.actions.ts)milestone5, add action  for fetch todo-items

--- src/app/ngxs/todo-items/todo-items.actions.ts
+++ src/app/ngxs/todo-items/todo-items.actions.ts
@@ -1,1 +1,25 @@
+import { HttpErrorResponse } from '@angular/common/http';
 
+import { TodoItem } from '../../core/models/todo-item';
+
+export class FetchTodoItems {
+  static readonly type = '[Todo Items] Fetch Todo Item';
+}
+
+export class FetchTodoItemSuccessfully {
+  static readonly type = '[Todo Items] Fetch Todo Item Successfully';
+
+  constructor(
+    public readonly payload: TodoItem[]
+  ) {
+  }
+}
+
+export class FetchTodoItemFailed {
+  static readonly type = '[Todo Items] Fetch Todo Item Failed';
+
+  constructor(
+    public readonly payload: HttpErrorResponse
+  ) {
+  }
+}
